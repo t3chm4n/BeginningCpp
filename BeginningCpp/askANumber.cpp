789e0eb7c5690f5d6293db88baf350f6d46e40d0
maikon neitzel <maikonbr@gmail.com>
2015-12-11 20:26:02 -0200

    Add askANumber (Chapter 5 - Exercise 3)

--- BeginningCpp/askANumber.cpp
+++ BeginningCpp/askANumber.cpp
@@ -1,7 +1,18 @@
 #include "askANumber.h"
 
+using namespace std;
+
+int returnNumber(string msg);
+
 void askANumber()
 {
-
+	cout << "The chosen number was " << returnNumber("Enter a integer: ") << endl;
 }
 
+int returnNumber(string msg = "Enter a number: ")
+{
+	cout << msg;
+	int number;
+	cin >> number;
+	return number;
+}
